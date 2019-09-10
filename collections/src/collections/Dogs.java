4e5df4b4507239eb30759a533e93b0c15f21179f
jeffyjkang <jeffyjkang@gmail.com>
2019-09-10 12:47:36 -0700

    update comments for collections

--- collections/src/collections/Dogs.java
+++ collections/src/collections/Dogs.java
@@ -1,55 +1,44 @@
 package collections;
 
-public class Dogs
-{    
+public class Dogs {
+    // fields - attributes - values of all fields (state of the object)
     private String breed;
     private int avgWeight;
     private boolean apartment;
 
-    public Dogs(String breed, int avgWeight, boolean apartment)
-    {
+    // constructor
+    public Dogs(String breed, int avgWeight, boolean apartment) {
         this.breed = breed;
         this.avgWeight = avgWeight;
         this.apartment = apartment;
     }
 
-    public String getBreed()
-    {
+    public String getBreed() {
         return breed;
     }
 
-    public void setBreed(String breed)
-    {
+    public void setBreed(String breed) {
         this.breed = breed;
     }
 
-    public int getAvgWeight()
-    {
+    public int getAvgWeight() {
         return avgWeight;
     }
 
-    public void setAvgWeight(int avgWeight)
-    {
+    public void setAvgWeight(int avgWeight) {
         this.avgWeight = avgWeight;
     }
 
-    public boolean isApartment()
-    {
+    public boolean isApartment() {
         return apartment;
     }
 
-    public void setApartment(boolean apartment)
-    {
+    public void setApartment(boolean apartment) {
         this.apartment = apartment;
     }
 
     @Override
-    public String toString()
-    {
-        return "Dogs{" +
-                "breed='" + breed + '\'' +
-                ", avgWeight=" + avgWeight +
-                ", apartment=" + apartment +
-                '}';
+    public String toString() {
+        return "Dogs{" + "breed='" + breed + '\'' + ", avgWeight=" + avgWeight + ", apartment=" + apartment + '}';
     }
 }
